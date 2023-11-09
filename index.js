3e1a5301e006e6d50cab49203d90fae0e2d671e9
Oshomah <oshomah26@gmail.com>
2023-11-09 18:21:41 +0100

    made the carousel in CSS without the use of radio buttons

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const global = {
     currentPage: window.location.pathname
 }
 
+alert("Bing!")
+
+document.querySelectorAll("carousel").forEach(carousel => {
+    const items = carousel.querySelectorAll(".carousel_item");
+    const buttonsHtml = Array.from(items, () => {
+        return `<span class="carousel_button"> </span>`;
+    });
+
+    console.log(buttonsHtml);
+})
+
+
+
 //Init App 
 // function init() {
 //     switch (global.currentPage) {
